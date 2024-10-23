ea065407f2ee03909814544bcc6cbe55807a4561
noellvt <vtnoell@gmail.com>
2024-10-23 10:17:31 +0700

    feat: versi code baru

--- TugasSorting.cpp
+++ TugasSorting.cpp
@@ -3,7 +3,7 @@ Nama         : Fardan Fadhilah Andicha Putra
 NPM          : 140810240084
 Bagian       : Bubble sort cara ke-1 dan shell sort
 Tanggal Buat : 21 Oktober 2024
-Deskripsi    : 
+Deskripsi    : Menyorting angka secara ascending dan descending dengan menggunakan bubble sort cara ke-1 dan shell sort 
 
 Nama         : Tristan Bonardo Silalahi
 NPM          : 140810240058
@@ -15,290 +15,442 @@ Nama         : Noell Valentino Timothy
 NPM          : 140810240064
 Bagian       : Bubble sort cara ke-3 dan insertion sort
 Tanggal Buat : 21 Oktober 2024
-Deskripsi    : 
+Deskripsi    : Menyorting angka secara ascending dan descending dengan menggunakan bubble sort cara ke-3 dan insertion sort 
 */
+
 
 #include <iostream>
 using namespace std;
 
 // Deklarasi array dengan alias
-typedef int larik[30];
+typedef int larik[50];
 
 
 // Deklarasi Fungsi
-int inputBanyakData(int banyakData);
-void inputData(int& banyakData, larik kumpulanData);
-void menu(int banyakData, larik kumpulanData);
-void tukar(int& x, int& y);
-void cetakLarik(larik kumpulanData, int n);
-
-void bubble1Asc(int banyakData, larik kumpulanData);
-void bubble1Des(int banyakData, larik kumpulanData);
-void bubble2Asc(int banyakData, larik kumpulanData);
-void bubble2Des(int banyakData, larik kumpulanData);
-void bubble3Asc(int banyakData, larik kumpulanData);
-void bubble3Des(int banyakData, larik kumpulanData);
-
-void insertionAsc(int banyakData, larik kumpulanData);
-void insertionDes(int banyakData, larik kumpulanData);
-
-void selectionAsc(int banyakData, larik kumpulanData);
-void selectionDes(int banyakData, larik kumpulanData);
-
-void shellAsc(int banyakData, larik kumpulanData);
-void shellDes(int banyakData, larik kumpulanData);
+void inputBanyak(int& n);
+void inputData(larik arr,int n);
+void tukar(int &x, int &y);
+void cetakLarik(larik arr,int n);
+void bubbleSort1Asc(larik arr, int n);
+void bubbleSort1Des(larik arr, int n);
+void bubbleSort2Asc(larik arr, int n);
+void bubbleSort2Des(larik arr, int n);
+void bubbleSort3Asc (larik arr, int n);
+void bubbleSort3Des (larik arr, int n);
+void selectionAsc(larik arr, int n);
+void selectionDes(larik arr, int n);
+void insertionAsc(larik arr, int n);
+void insertionDes(larik arr, int n);
+void shellAsc (larik arr, int n);
+void shellDes (larik arr, int n);
+void menuAtas();
+void menuBawahAsc();
+void menuBawahDes();
 
 
 // Utama
 int main()
 {
-    int banyakData = inputBanyakData(banyakData);
-
-    larik kumpulanData;
-    inputData(banyakData, kumpulanData);
-
-    menu(banyakData, kumpulanData);
+    menuAtas();
     return 0;
 }
 
 
 // Pendefinisian Fungsi
-void inputData(int& banyakData, larik kumpulanData) {
-    cout << "Masukkan " << banyakData << " jumlah data untuk disortir\t: \n";
-    for (int i = 0; i < banyakData; i++)
-    {
-        cin >> kumpulanData[i];
+void inputBanyak(int& n){
+    cout<<"Masukan banyak angka yang ingin disort :";
+    cin>>n;
+}
+
+void inputData(larik arr,int n){
+    cout<<"Masukan angka sebanyak "<<n<<" :"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
     }
 }
 
-
-int inputBanyakData(int banyakData) {
-    cout << "Masukkan banyak data yang ingin diolah\t: ";
-    cin >> banyakData;
-    return banyakData;
+void tukar (int &x, int &y){
+    int temp = x;
+    x = y;
+    y = temp;
 }
 
-
-void menu(int banyakData, larik kumpulanData) {
-    int urutan = 0, algoritma = 0;
-    string lanjut = "";
-
-    cout << "\nPilih bagaimana urutan data akan disortir : \n";
-    cout << "1. Ascending   (dari paling kecil) \n";
-    cout << "2. Descending  (dari paling besar) \n";
-    do {
-    cout << "Masukkan pilihan (1 - 2) : ";
-    cin >> urutan;
-    } while (urutan < 1 || urutan > 2);
-
-    cout << "\nPilih algoritma sorting yang akan digunakan : \n";
-    cout << "1. Bubble Sort (Cara 1)    \n";
-    cout << "2. Bubble Sort (Cara 2)    \n";
-    cout << "3. Bubble Sort (Cara 3)    \n";
-    cout << "4. Selection Sort          \n";
-    cout << "5. Insertion Sort          \n";
-    cout << "6. Shell Sort              \n";
-    do {
-    cout << "Masukkan pilihan (1 - 2) : ";
-    cin >> algoritma;
-    } while (algoritma < 1 || algoritma > 6);
-
-
-    switch (urutan)
-    {
-    case 1:
-        switch (algoritma) {
-        case 1 : bubble1Asc(banyakData, kumpulanData); break;
-        case 2 : bubble2Asc(banyakData, kumpulanData); break;
-        case 3 : bubble3Asc(banyakData, kumpulanData); break;
-        case 4 : selectionAsc(banyakData, kumpulanData); break;
-        case 5 : insertionAsc(banyakData, kumpulanData); break;
-        case 6 : shellAsc(banyakData, kumpulanData); break;
-        default: break;
-        }
-        break;
-
-    case 2:
-        switch (algoritma) {
-        case 1 : bubble1Des(banyakData, kumpulanData); break;
-        case 2 : bubble2Des(banyakData, kumpulanData); break;
-        case 3 : bubble3Des(banyakData, kumpulanData); break;
-        case 4 : selectionDes(banyakData, kumpulanData); break;
-        case 5 : insertionDes(banyakData, kumpulanData); break;
-        case 6 : shellDes(banyakData, kumpulanData); break;
-        default: break;
-        }
-        break;
+void cetakLarik(larik arr,int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
     }
-
-    do {
-        cout << "Apakah anda ingin mengurutkan data lagi? (Ya/Tidak) \n";
-        cin >> lanjut;
-    } while (lanjut != "Ya" && lanjut != "ya" && lanjut != "Tidak" && lanjut != "tidak");
-    if (lanjut == "Ya" || lanjut == "ya")
-        main();
 }
 
-void tukar(int& x, int& y) {
-    int temp = y;
-    y = x;
-    x = temp;
-}
-
-void cetakLarik(larik kumpulanData, int n) {
-    for (int i = 0; i < n; i++)
-        cout << kumpulanData[i] << " ";
-    cout << endl;
-}
-
-void bubble1Asc(int banyakData, larik kumpulanData) {
-    int proses = 1;
-    for (int i = 0; i < banyakData; i++) {
-        for (int j = 0; j < banyakData - 1; j++) {
-            if (kumpulanData[j] > kumpulanData[j + 1]) {
-                tukar(kumpulanData[j], kumpulanData[j + 1]);
-                cout << "Proses ke-" << proses++ << " : ";
-                cetakLarik(kumpulanData, banyakData);
+void bubbleSort1Asc(larik arr, int n){
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n-1; j++){
+            if (arr[j] > arr[j+1]){
+                tukar(arr[j],arr[j+1]);
+                cetakLarik (arr, n);
+                cout << endl;
             }
         }
     }
+    cout<<"Data terurut : "<<endl;
+    cetakLarik(arr,n);
+    cout<<endl;
 }
 
-void bubble1Des(int banyakData, larik kumpulanData) {
-    int proses = 1;
-    for (int i = 0; i < banyakData; i++) {
-        for (int j = 0; j < banyakData - 1; j++) {
-            if (kumpulanData[j] < kumpulanData[j + 1 ]) {
-                tukar(kumpulanData[j], kumpulanData[j + 1]);
-                cout << "Proses ke-" << proses++ << " : ";
-                cetakLarik(kumpulanData, banyakData);
+void bubbleSort1Des(larik arr, int n){
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n-1; j++){
+            if (arr[j] < arr[j+1]){
+                tukar(arr[j],arr[j+1]);
+                cetakLarik (arr, n);
+                cout << endl;
             }
         }
     }
+    cout<<"Data terurut : "<<endl;
+    cetakLarik(arr,n);
+    cout<<endl;
 }
 
-void bubble2Asc(int banyakData, larik kumpulanData) {
-    int proses = 1;
-    for(int i = 0; i < banyakData - 1; i++) {
-        for(int j = banyakData - 1; j > i; j--){
-            if(kumpulanData[j] < kumpulanData[j - 1]){
-                tukar(kumpulanData[j], kumpulanData[j - 1]);
-                cout << "Proses ke-" << proses++ << " : ";
-                cetakLarik(kumpulanData, banyakData);
+void bubbleSort2Asc(larik arr, int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=n-1;j>i;j--){
+            if(arr[j]<arr[j-1]){
+                tukar(arr[j],arr[j-1]);
+                cetakLarik(arr,n);
+                cout<<endl;
             }
         }
     }
+    cout<<"Data terurut : "<<endl;
+    cetakLarik(arr,n);
+    cout<<endl;
 }
 
-void bubble2Des(int banyakData, larik kumpulanData) {
-    int proses = 1;
-    for(int i = 0; i < banyakData - 1; i++){
-        for(int j = banyakData - 1; j > i; j--){
-            if(kumpulanData[j] > kumpulanData[j - 1]){
-                tukar(kumpulanData[j], kumpulanData[j - 1]);
-                cout << "Proses ke-" << proses++ << " : ";
-                cetakLarik(kumpulanData, banyakData);
+void bubbleSort2Des(larik arr, int n){
+
+    for(int i=0;i<n-1;i++){
+        for(int j=n-1;j>i;j--){
+            if(arr[j]>arr[j-1]){
+                tukar(arr[j],arr[j-1]);
+                cetakLarik(arr,n);
+                cout<<endl;
             }
         }
     }
+    cout<<"Data terurut : "<<endl;
+    cetakLarik(arr,n);
+    cout<<endl;
 }
 
-void bubble3Asc(int banyakData, larik kumpulanData) {
-    for (int i = 0; i < banyakData; i++) {
-        cout << "Proses ke-" << i + 1 << " : ";
-        for (int j = i + 1; j < banyakData; j++)
-            if (kumpulanData[i] > kumpulanData[j]) tukar(kumpulanData[i], kumpulanData[j]);
-        cetakLarik(kumpulanData, banyakData);
+void bubbleSort3Des (larik arr, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++)
+            if (arr[i] < arr[j]){ 
+            tukar(arr[i], arr[j]);
+            cetakLarik(arr, n);
+            cout<<endl;
+            }
     }
+    cout<<"Data terurut : "<<endl;
+    cetakLarik(arr,n);
 }
 
-void bubble3Des(int banyakData, larik kumpulanData) {
-    for (int i = 0; i < banyakData; i++) {
-        cout << "Proses ke-" << i + 1 << " : ";
-        for (int j = i + 1; j < banyakData; j++)
-            if (kumpulanData[i] < kumpulanData[j]) tukar(kumpulanData[i], kumpulanData[j]);
-        cetakLarik(kumpulanData, banyakData);
+void bubbleSort3Asc (larik arr, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++)
+            if (arr[i] > arr[j]){
+            tukar(arr[i], arr[j]);
+            cetakLarik(arr, n);
+            cout<<endl;
+            }
     }
+    cout<<"Data terurut : "<<endl;
+    cetakLarik(arr,n);
+    cout<<endl;
 }
 
-void insertionAsc(int banyakData, larik kumpulanData) {
-    for (int i = 1; i < banyakData; i++) {
-        cout << "Proses ke-" << i + 1 << " : ";
-        for (int j = i - 1; j >= 0; j--) {
-            if (kumpulanData[j] > kumpulanData[j + 1])
-                tukar(kumpulanData[j], kumpulanData[j + 1]);
-        }
-        cetakLarik(kumpulanData, banyakData);
-    }
-}
+void selectionAsc(larik arr, int n) {
+    int posisi;
+    bool benar;
 
-void insertionDes(int banyakData, larik kumpulanData) {
-    for (int i = 1; i < banyakData; i++) {
-        cout << "Proses ke-" << i + 1 << " : ";
-        for (int j = i - 1; j >= 0; j--) {
-            if (kumpulanData[j] < kumpulanData[j + 1])
-                tukar(kumpulanData[j], kumpulanData[j + 1]);
-        }
-        cetakLarik(kumpulanData, banyakData);
-    }
-}
-
-void selectionAsc(int banyakData, larik kumpulanData) {
-    int posisi = 0;
-    for(int i = 0; i < banyakData - 1; i++){
-        cout << "Proses ke-" << i + 1 << " : ";
+    for (int i = 0; i < n - 1; i++) {
         posisi = i;
-        for(int j = i + 1; j < banyakData; j++){
-            if(kumpulanData[posisi] > kumpulanData[j]){
+        benar = false;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[posisi] > arr[j]) {
                 posisi = j;
             }
         }
-        tukar(kumpulanData[i], kumpulanData[posisi]);
-        cetakLarik(kumpulanData, banyakData);
+
+        if (posisi != i) {
+            tukar(arr[i], arr[posisi]);
+            benar = true;
+            cetakLarik(arr, n);
+            cout<<endl;
+        }
+
+        if (!benar) {
+            break;
+        }
     }
+    cout<<"Data terurut : ";cetakLarik(arr,n);
+    cout<<endl;
 }
 
-void selectionDes(int banyakData, larik kumpulanData) {
-    int posisi = 0;
-    for(int i = 0; i < banyakData - 1; i++){
-        cout << "Proses ke-" << i + 1 << " : ";
+
+void selectionDes(larik arr, int n) {
+    int posisi;
+    bool benar;
+
+    for (int i = 0; i < n - 1; i++) {
         posisi = i;
-        for(int j = i + 1; j < banyakData; j++){
-            if(kumpulanData[posisi] < kumpulanData[j]) {
+        benar = false;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[posisi] < arr[j]) {
                 posisi = j;
             }
         }
-        tukar(kumpulanData[i], kumpulanData[posisi]);
-        cetakLarik(kumpulanData, banyakData);
-    }
-}
 
-void shellAsc(int banyakData, larik kumpulanData) {
-    int j = 0, proses = 1;
-    for (int jarak = banyakData / 2; jarak > 0; jarak /= 2){
-        for (int i = jarak; i < banyakData; i++){
-            cout << "Proses ke-" << proses++ << " : ";
-            int temp = kumpulanData[i];
-            for (j = i; j >= jarak && temp < kumpulanData[j - jarak]; j -= jarak){
-                kumpulanData[j] = kumpulanData[j - jarak];
-            }
-            kumpulanData[j] = temp;
-            cetakLarik (kumpulanData, banyakData);
+        if (posisi != i) {
+            tukar(arr[i], arr[posisi]);
+            benar = true;
+            cetakLarik(arr, n);
+            cout<<endl;
+        }
+
+        if (!benar) {
+            break;
         }
     }
+    cout<<"Data terurut : ";cetakLarik(arr,n);
+    cout<<endl;
 }
 
-void shellDes(int banyakData, larik kumpulanData) {
-    int j = 0, proses = 1;
-    for (int jarak = banyakData / 2; jarak > 0; jarak /= 2){
-        for (int i = jarak; i < banyakData; i++){
-            cout << "Proses ke-" << proses++ << " : ";
-            int temp = kumpulanData[i];
-            for (j = i; j >= jarak && temp > kumpulanData[j - jarak]; j -= jarak){
-                kumpulanData[j] = kumpulanData[j - jarak];
+void insertionAsc(larik arr, int n) {
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = i - 1; j >= 0; j--)
+        {
+            if (arr[j] > arr[j + 1]) {
+                tukar(arr[j], arr[j + 1]);
             }
-            kumpulanData[j] = temp;
-            cetakLarik (kumpulanData, banyakData);
+        }
+        cetakLarik(arr, n);
+        cout<<endl;
+    }
+    cout<<"Data terurut : ";cetakLarik(arr,n);
+    cout<<endl;
+}
+
+void insertionDes(larik arr, int n) {
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = i - 1; j >= 0; j--)
+        {
+            if (arr[j] < arr[j + 1]) {
+                tukar(arr[j], arr[j + 1]);
+            }
+        }
+        cetakLarik(arr, n);
+        cout<<endl;
+    }
+    cout<<"Data terurut : ";cetakLarik(arr,n);
+    cout<<endl;
+}
+
+void shellAsc (larik arr, int n){
+    int j;
+    for (int jarak=n/2; jarak>0; jarak/=2){
+        for (int i=jarak; i<n; i++){
+            int temp = arr[i];
+            for (j=i; j>=jarak && temp < arr[j-jarak]; j-=jarak){
+                arr[j] = arr[j-jarak];
+            }
+            arr[j]=temp;
+            cetakLarik (arr,n);
+            cout << "\n";
         }
     }
+    cout<<"Data terurut : ";cetakLarik(arr,n);
+    cout<<endl;
+}
+
+void shellDes (larik arr, int n){
+    int j;
+    for (int jarak=n/2; jarak>0; jarak/=2){
+        for (int i=jarak; i<n; i++){
+            int temp = arr[i];
+            for (j=i; j>=jarak && temp > arr[j-jarak]; j-=jarak){
+                arr[j] = arr[j-jarak];
+            }
+            arr[j]=temp;
+            cetakLarik (arr,n);
+            cout << "\n";
+        }
+    }
+    cout<<"Data terurut : ";cetakLarik(arr,n);
+    cout<<endl;
+}
+
+void menuAtas(){
+    bool menuA = true;
+    int pilihan;
+    do
+    {
+        cout<<"1. Mengurutkan secara ascending"<<endl;
+        cout<<"2. Mengurutkan secara descending"<<endl;
+        cout<<"3. Keluar"<<endl;
+        cout<<" Masukan pilihan : ";cin>>pilihan;
+        switch (pilihan)
+        {
+        case 1:
+            menuBawahAsc();
+            break;
+        
+        case 2:
+            menuBawahDes();
+            break;
+
+        case 3:
+            menuA = false;
+            break;
+
+        default:
+        cout<<"pilihan tidak valid!"<<endl;
+            break;
+        }
+    } while (menuA == true);
+    
+}
+
+void menuBawahAsc(){
+    bool menuBA = true;
+    int pilihanA;
+    int n;
+    larik arr;
+    do
+    {
+        cout<<"1. Bubble Sort cara 1"<<endl;
+        cout<<"2. Bubble Sort cara 2"<<endl;
+        cout<<"3. Bubble Sort cara 3"<<endl;
+        cout<<"4. Selection Sort"<<endl;
+        cout<<"5. Insertion Sort"<<endl;
+        cout<<"6. Shell Sort"<<endl;
+        cout<<"7. Mundur"<<endl;
+        cout<<"Masukan pilihan : ";cin>>pilihanA;
+        cout<<endl;
+        switch (pilihanA)
+        {
+        case 1:
+            inputBanyak(n);
+            inputData(arr,n);
+            bubbleSort1Asc(arr,n);
+            cout<<endl;
+            break;
+        case 2:
+            inputBanyak(n);
+            inputData(arr,n);
+            bubbleSort2Asc(arr,n);
+            cout<<endl;
+            break;
+        case 3:
+            inputBanyak(n);
+            inputData(arr,n);
+            bubbleSort3Asc(arr,n);
+            cout<<endl;
+            break;
+        case 4:
+            inputBanyak(n);
+            inputData(arr,n);
+            selectionAsc(arr,n);
+            cout<<endl;
+            break;
+        case 5:
+            inputBanyak(n);
+            inputData(arr,n);
+            insertionAsc(arr,n);
+            cout<<endl;
+            break;
+        case 6:
+            inputBanyak(n);
+            inputData(arr,n);
+            shellAsc(arr,n);
+            cout<<endl;
+            break;
+            case 7:
+            menuBA = false;
+            break;
+        default:
+        cout<<"input tidak valid !"<<endl;
+            break;
+        }
+    } while (menuBA == true);
+    
+}
+
+void menuBawahDes(){
+    bool menuBD = true;
+    int pilihanD;
+    int n;
+    larik arr;
+    do
+    {
+        cout<<"1. Bubble Sort cara 1"<<endl;
+        cout<<"2. Bubble Sort cara 2"<<endl;
+        cout<<"3. Bubble Sort cara 3"<<endl;
+        cout<<"4. Selection Sort"<<endl;
+        cout<<"5. Insertion Sort"<<endl;
+        cout<<"6. Shell Sort"<<endl;
+        cout<<"7. Mundur"<<endl;
+        cout<<"Masukan pilihan : ";cin>>pilihanD;
+        cout<<endl;
+        switch (pilihanD)
+        {
+        case 1:
+            inputBanyak(n);
+            inputData(arr,n);
+            bubbleSort1Des(arr,n);
+            cout<<endl;
+            break;
+        case 2:
+            inputBanyak(n);
+            inputData(arr,n);
+            bubbleSort2Des(arr,n);
+            cout<<endl;
+            break;
+        case 3:
+            inputBanyak(n);
+            inputData(arr,n);
+            bubbleSort3Des(arr,n);
+            cout<<endl;
+            break;
+        case 4:
+            inputBanyak(n);
+            inputData(arr,n);
+            selectionDes(arr,n);
+            cout<<endl;
+            break;
+        case 5:
+            inputBanyak(n);
+            inputData(arr,n);
+            insertionDes(arr,n);
+            cout<<endl;
+            break;
+        case 6:
+            inputBanyak(n);
+            inputData(arr,n);
+            shellDes(arr,n);
+            cout<<endl;
+            break;
+            case 7:
+            menuBD = false;
+            break;
+        default:
+        cout<<"input tidak valid !"<<endl;
+            break;
+        }
+    } while (menuBD == true);
+    
 }
