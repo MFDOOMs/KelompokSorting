b0f004724edb5257e69c6293c30804ec93d3e1ad
noellvt <vtnoell@gmail.com>
2024-10-29 13:42:43 +0700

    merapihkan

--- TugasSorting.cpp
+++ TugasSorting.cpp
@@ -2,19 +2,19 @@
 Nama         : Fardan Fadhilah Andicha Putra
 NPM          : 140810240084
 Bagian       : Bubble sort cara ke-1 dan shell sort
-Tanggal Buat : 21 Oktober 2024
+Tanggal Buat : 28 Oktober 2024
 Deskripsi    : Menyorting angka secara ascending dan descending dengan menggunakan bubble sort cara ke-1 dan shell sort 
 
 Nama         : Tristan Bonardo Silalahi
 NPM          : 140810240058
 Bagian       : Bubble sort cara ke-2 dan selection sort
-Tanggal Buat : 21 Oktober 2024
+Tanggal Buat : 28 Oktober 2024
 Deskripsi    : Menyorting angka secara ascending dan descending dengan menggunakan bubble sort cara ke-2 dan selection sort 
 
 Nama         : Noell Valentino Timothy
 NPM          : 140810240064
 Bagian       : Bubble sort cara ke-3 dan insertion sort
-Tanggal Buat : 21 Oktober 2024
+Tanggal Buat : 28 Oktober 2024
 Deskripsi    : Menyorting angka secara ascending dan descending dengan menggunakan bubble sort cara ke-3 dan insertion sort 
 */
 
@@ -22,15 +22,16 @@ Deskripsi    : Menyorting angka secara ascending dan descending dengan menggunak
 #include <iostream>
 using namespace std;
 
+
 // Deklarasi array dengan alias
 typedef char larik[50];
 
 
 // Deklarasi Fungsi
 void inputBanyak(int& n);
-void inputData(larik arr,int n);
-void tukar(char &x, char &y);
-void cetakLarik(larik arr,int n);
+void inputData(larik arr, int n);
+void tukar(char& x, char& y);
+void cetakLarik(larik arr, int n);
 void bubbleSort1Asc(larik arr, int n);
 void bubbleSort1Des(larik arr, int n);
 void bubbleSort2Asc(larik arr, int n);
@@ -46,8 +47,8 @@ void shellDes (larik arr, int n);
 void menuSort();
 void menuBawahAsc();
 void menuBawahDes();
-void linearSearch();
-void binarySearch();
+void linearSearch(larik arr, int n, char x);
+void binarySearch(larik arr, int n, char x);
 void menuUtama();
 void menuSearch();
 
@@ -61,27 +62,250 @@ int main()
 
 
 // Pendefinisian Fungsi
+void menuUtama() {
+    bool MenuU = true;
+    int pilihan;
+    do
+    {
+        cout << "1. Sorting" << endl;
+        cout << "2. Searching" << endl;
+        cout << "3. Keluar" << endl;
+        cout << "Masukan Pilihan : ";
+        cin >> pilihan;
+        switch (pilihan)
+        {
+        case 1:
+            menuSort();
+            break;
+        case 2:
+            menuSearch();
+            break;
+        case 3:
+            MenuU =false;
+            break;
+        default:
+            cout<<"pilihan tidak valid";
+            break;
+        }
+    } while (MenuU);
+}
+
+void menuSort(){
+    bool menuA = true;
+    int pilihan;
+    do
+    {
+        cout<<"1. Mengurutkan secara ascending"<<endl;
+        cout<<"2. Mengurutkan secara descending"<<endl;
+        cout<<"3. Keluar"<<endl;
+        cout<<" Masukan pilihan : ";cin>>pilihan;
+        switch (pilihan)
+        {
+        case 1:
+            menuBawahAsc();
+            break;
+        
+        case 2:
+            menuBawahDes();
+            break;
+
+        case 3:
+            menuA = false;
+            break;
+
+        default:
+        cout<<"pilihan tidak valid!"<<endl;
+            break;
+        }
+    } while (menuA == true);
+}
+
+void menuSearch(){
+    bool menuS;
+    int pilihan;
+    int n;
+    larik arr;
+    char x;
+    do
+    {
+        cout<<"1. Linear search"<<endl;
+        cout<<"2. Binary Search"<<endl;
+        cout<<"3. Mundur"<<endl;
+        cout<< "Masukkan pilihan : ";
+        cin >> pilihan;
+        switch (pilihan)
+        {
+        case 1:
+            inputBanyak(n);
+            inputData(arr,n);
+            linearSearch(arr,n,x);
+            break;
+        case 2:
+            inputBanyak(n);
+            inputData(arr,n);
+            bubbleSort1Asc(arr,n);
+            binarySearch(arr,n,x);
+            break;
+        case 3:
+            menuS = false;
+            break;
+        
+        default:
+            cout<<"Pilihan tidak valid"<<endl;
+            break;
+        }
+    } while (menuS);
+}
+
+void menuBawahAsc(){
+    bool menuBA = true;
+    int pilihanA;
+    int n;
+    larik arr;
+    do
+    {
+        cout<<"1. Bubble Sort cara 1"<<endl;
+        cout<<"2. Bubble Sort cara 2"<<endl;
+        cout<<"3. Bubble Sort cara 3"<<endl;
+        cout<<"4. Selection Sort"<<endl;
+        cout<<"5. Insertion Sort"<<endl;
+        cout<<"6. Shell Sort"<<endl;
+        cout<<"7. Mundur"<<endl;
+        cout<<"Masukan pilihan : ";cin>>pilihanA;
+        cout<<endl;
+        switch (pilihanA)
+        {
+        case 1:
+            inputBanyak(n);
+            inputData(arr,n);
+            bubbleSort1Asc(arr,n);
+            cout<<endl;
+            break;
+        case 2:
+            inputBanyak(n);
+            inputData(arr,n);
+            bubbleSort2Asc(arr,n);
+            cout<<endl;
+            break;
+        case 3:
+            inputBanyak(n);
+            inputData(arr,n);
+            bubbleSort3Asc(arr,n);
+            cout<<endl;
+            break;
+        case 4:
+            inputBanyak(n);
+            inputData(arr,n);
+            selectionAsc(arr,n);
+            cout<<endl;
+            break;
+        case 5:
+            inputBanyak(n);
+            inputData(arr,n);
+            insertionAsc(arr,n);
+            cout<<endl;
+            break;
+        case 6:
+            inputBanyak(n);
+            inputData(arr,n);
+            shellAsc(arr,n);
+            cout<<endl;
+            break;
+        case 7:
+            menuBA = false;
+            break;
+        default:
+        cout<<"input tidak valid !"<<endl;
+            break;
+        }
+    } while (menuBA == true);   
+}
+
+void menuBawahDes(){
+    bool menuBD = true;
+    int pilihanD;
+    int n;
+    larik arr;
+    do
+    {
+        cout<<"1. Bubble Sort cara 1"<<endl;
+        cout<<"2. Bubble Sort cara 2"<<endl;
+        cout<<"3. Bubble Sort cara 3"<<endl;
+        cout<<"4. Selection Sort"<<endl;
+        cout<<"5. Insertion Sort"<<endl;
+        cout<<"6. Shell Sort"<<endl;
+        cout<<"7. Mundur"<<endl;
+        cout<<"Masukan pilihan : ";cin>>pilihanD;
+        cout<<endl;
+        switch (pilihanD)
+        {
+        case 1:
+            inputBanyak(n);
+            inputData(arr,n);
+            bubbleSort1Des(arr,n);
+            cout<<endl;
+            break;
+        case 2:
+            inputBanyak(n);
+            inputData(arr,n);
+            bubbleSort2Des(arr,n);
+            cout<<endl;
+            break;
+        case 3:
+            inputBanyak(n);
+            inputData(arr,n);
+            bubbleSort3Des(arr,n);
+            cout<<endl;
+            break;
+        case 4:
+            inputBanyak(n);
+            inputData(arr,n);
+            selectionDes(arr,n);
+            cout<<endl;
+            break;
+        case 5:
+            inputBanyak(n);
+            inputData(arr,n);
+            insertionDes(arr,n);
+            cout<<endl;
+            break;
+        case 6:
+            inputBanyak(n);
+            inputData(arr,n);
+            shellDes(arr,n);
+            cout<<endl;
+            break;
+            case 7:
+            menuBD = false;
+            break;
+        default:
+        cout<<"input tidak valid !"<<endl;
+            break;
+        }
+    } while (menuBD == true);
+}
+
 void inputBanyak(int& n){
-    cout<<"Masukan banyak angka : ";
-    cin>>n;
+    cout << "Masukan banyak angka : ";
+    cin >> n;
 }
 
 void inputData(larik arr,int n){
-    cout<<"Masukan angka sebanyak "<<n<<" : "<<endl;
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    cout << "Masukan angka sebanyak " << n << " : " << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
 }
 
-void tukar (char &x, char &y){
+void tukar (char& x, char& y){
     char temp = x;
     x = y;
     y = temp;
 }
 
-void cetakLarik(larik arr,int n){
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+void cetakLarik(larik arr, int n){
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
     }
 }
 
@@ -131,7 +355,6 @@ void bubbleSort2Asc(larik arr, int n){
 }
 
 void bubbleSort2Des(larik arr, int n){
-
     for(int i=0;i<n-1;i++){
         for(int j=n-1;j>i;j--){
             if(arr[j]>arr[j-1]){
@@ -198,7 +421,8 @@ void selectionAsc(larik arr, int n) {
             break;
         }
     }
-    cout<<"Data terurut : ";cetakLarik(arr,n);
+    cout << "Data terurut : " << endl;
+    cetakLarik(arr,n);
     cout<<endl;
 }
 
@@ -228,7 +452,8 @@ void selectionDes(larik arr, int n) {
             break;
         }
     }
-    cout<<"Data terurut : ";cetakLarik(arr,n);
+    cout << "Data terurut : " << endl;
+    cetakLarik(arr,n);
     cout<<endl;
 }
 
@@ -244,7 +469,8 @@ void insertionAsc(larik arr, int n) {
         cetakLarik(arr, n);
         cout<<endl;
     }
-    cout<<"Data terurut : ";cetakLarik(arr,n);
+    cout << "Data terurut : " << endl;
+    cetakLarik(arr,n);
     cout<<endl;
 }
 
@@ -260,7 +486,8 @@ void insertionDes(larik arr, int n) {
         cetakLarik(arr, n);
         cout<<endl;
     }
-    cout<<"Data terurut : ";cetakLarik(arr,n);
+    cout << "Data terurut : " << endl;
+    cetakLarik(arr,n);
     cout<<endl;
 }
 
@@ -277,7 +504,8 @@ void shellAsc (larik arr, int n){
             cout << "\n";
         }
     }
-    cout<<"Data terurut : ";cetakLarik(arr,n);
+    cout << "Data terurut : " << endl;
+    cetakLarik(arr,n);
     cout<<endl;
 }
 
@@ -294,172 +522,12 @@ void shellDes (larik arr, int n){
             cout << "\n";
         }
     }
-    cout<<"Data terurut : ";cetakLarik(arr,n);
+    cout << "Data terurut : " << endl;
+    cetakLarik(arr,n);
     cout<<endl;
 }
 
-void menuSort(){
-    bool menuA = true;
-    int pilihan;
-    do
-    {
-        cout<<"1. Mengurutkan secara ascending"<<endl;
-        cout<<"2. Mengurutkan secara descending"<<endl;
-        cout<<"3. Keluar"<<endl;
-        cout<<" Masukan pilihan : ";cin>>pilihan;
-        switch (pilihan)
-        {
-        case 1:
-            menuBawahAsc();
-            break;
-        
-        case 2:
-            menuBawahDes();
-            break;
-
-        case 3:
-            menuA = false;
-            break;
-
-        default:
-        cout<<"pilihan tidak valid!"<<endl;
-            break;
-        }
-    } while (menuA == true);
-    
-}
-
-void menuBawahAsc(){
-    bool menuBA = true;
-    int pilihanA;
-    int n;
-    larik arr;
-    do
-    {
-        cout<<"1. Bubble Sort cara 1"<<endl;
-        cout<<"2. Bubble Sort cara 2"<<endl;
-        cout<<"3. Bubble Sort cara 3"<<endl;
-        cout<<"4. Selection Sort"<<endl;
-        cout<<"5. Insertion Sort"<<endl;
-        cout<<"6. Shell Sort"<<endl;
-        cout<<"7. Mundur"<<endl;
-        cout<<"Masukan pilihan : ";cin>>pilihanA;
-        cout<<endl;
-        switch (pilihanA)
-        {
-        case 1:
-            inputBanyak(n);
-            inputData(arr,n);
-            bubbleSort1Asc(arr,n);
-            cout<<endl;
-            break;
-        case 2:
-            inputBanyak(n);
-            inputData(arr,n);
-            bubbleSort2Asc(arr,n);
-            cout<<endl;
-            break;
-        case 3:
-            inputBanyak(n);
-            inputData(arr,n);
-            bubbleSort3Asc(arr,n);
-            cout<<endl;
-            break;
-        case 4:
-            inputBanyak(n);
-            inputData(arr,n);
-            selectionAsc(arr,n);
-            cout<<endl;
-            break;
-        case 5:
-            inputBanyak(n);
-            inputData(arr,n);
-            insertionAsc(arr,n);
-            cout<<endl;
-            break;
-        case 6:
-            inputBanyak(n);
-            inputData(arr,n);
-            shellAsc(arr,n);
-            cout<<endl;
-            break;
-        case 7:
-            menuBA = false;
-            break;
-        default:
-        cout<<"input tidak valid !"<<endl;
-            break;
-        }
-    } while (menuBA == true);
-    
-}
-
-void menuBawahDes(){
-    bool menuBD = true;
-    int pilihanD;
-    int n;
-    larik arr;
-    do
-    {
-        cout<<"1. Bubble Sort cara 1"<<endl;
-        cout<<"2. Bubble Sort cara 2"<<endl;
-        cout<<"3. Bubble Sort cara 3"<<endl;
-        cout<<"4. Selection Sort"<<endl;
-        cout<<"5. Insertion Sort"<<endl;
-        cout<<"6. Shell Sort"<<endl;
-        cout<<"7. Mundur"<<endl;
-        cout<<"Masukan pilihan : ";cin>>pilihanD;
-        cout<<endl;
-        switch (pilihanD)
-        {
-        case 1:
-            inputBanyak(n);
-            inputData(arr,n);
-            bubbleSort1Des(arr,n);
-            cout<<endl;
-            break;
-        case 2:
-            inputBanyak(n);
-            inputData(arr,n);
-            bubbleSort2Des(arr,n);
-            cout<<endl;
-            break;
-        case 3:
-            inputBanyak(n);
-            inputData(arr,n);
-            bubbleSort3Des(arr,n);
-            cout<<endl;
-            break;
-        case 4:
-            inputBanyak(n);
-            inputData(arr,n);
-            selectionDes(arr,n);
-            cout<<endl;
-            break;
-        case 5:
-            inputBanyak(n);
-            inputData(arr,n);
-            insertionDes(arr,n);
-            cout<<endl;
-            break;
-        case 6:
-            inputBanyak(n);
-            inputData(arr,n);
-            shellDes(arr,n);
-            cout<<endl;
-            break;
-            case 7:
-            menuBD = false;
-            break;
-        default:
-        cout<<"input tidak valid !"<<endl;
-            break;
-        }
-    } while (menuBD == true);
-    
-}
-
-void linearSearch(larik arr, int n, char x){
+void linearSearch(larik arr, int n, char x) {
     cout << "Masukkan angka yang ingin dicari : ";
     cin >> x;
     bool found = false; 
@@ -505,68 +573,4 @@ void binarySearch(larik arr, int n, char x) {
     if (!found){
         cout << "Angka tidak ditemukan." << endl;
     }
-}
-void menuSearch(){
-    bool menuS;
-    int pilihan;
-    int n;
-    larik arr;
-    char x;
-    do
-    {
-        cout<<"1. Linear search"<<endl;
-        cout<<"2. Binary Search"<<endl;
-        cout<<"3. Mundur"<<endl;
-        cout<< "Masukkan pilihan : ";
-        cin >> pilihan;
-        switch (pilihan)
-        {
-        case 1:
-            inputBanyak(n);
-            inputData(arr,n);
-            linearSearch(arr,n,x);
-            break;
-        case 2:
-            inputBanyak(n);
-            inputData(arr,n);
-            bubbleSort1Asc(arr,n);
-            binarySearch(arr,n,x);
-            break;
-        case 3:
-            menuS = false;
-            break;
-        
-        default:
-            cout<<"Pilihan tidak valid"<<endl;
-            break;
-        }
-    } while (menuS);
-}
-
-void menuUtama(){
-    bool MenuU = true;
-    int pilihan;
-    do
-    {
-        cout << "1. Sorting" << endl;
-        cout << "2. Searching" << endl;
-        cout << "3. Keluar" << endl;
-        cout << "Masukan Pilihan : ";
-        cin >> pilihan;
-        switch (pilihan)
-        {
-        case 1:
-            menuSort();
-            break;
-        case 2:
-            menuSearch();
-            break;
-        case 3:
-            MenuU =false;
-            break;
-        default:
-            cout<<"pilihan tidak valid";
-            break;
-        }
-    } while (MenuU);
 }
