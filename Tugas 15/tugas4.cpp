a9215d3f21337e445bddb0a8b18c99ff021866bb
Fardan Fadhilah Andicha Putra <fardan24003@mail.unpad.ac.id>
2024-12-06 13:29:55 +0700

    update

--- Tugas 15/tugas4.cpp
+++ Tugas 15/tugas4.cpp
@@ -4,23 +4,23 @@ using namespace std;
 
 typedef char larik[100];
 
-void enkripsi (string inFile, string outFile){
+void enkripsi (string inFile, int geser){
     char karakter;
     larik encript;
     int i = 0;
     ifstream fi;
     ofstream fo;
     fi.open (inFile);
-    fo.open (outFile);
+    fo.open ("tugas4_output.txt");
     if (!fi){
         cerr << "File tidak ada.";
     }
     while (fi.get(karakter)){
-        if (karakter >= 65 && karakter <= 88){
-            karakter += 2;
+        if (karakter >= 65 && karakter <= 90-geser){
+            karakter += geser;
         }
-        else if (karakter >=89 && karakter <= 90){
-            karakter -= 24;
+        else if (karakter > 90-geser && karakter <= 90){
+            karakter -= 26-geser;
         }
         fo << karakter;
         i++;
@@ -30,8 +30,8 @@ void enkripsi (string inFile, string outFile){
 
 int main (){
     string inFile;
-    string outFile;
+    int geser;
     cout << "Masukkan nama file input: "; cin >> inFile;
-    cout << "Masukkan nama file output: "; cin >> outFile;
-    enkripsi (inFile, outFile);
+    cout << "Masukkan besar pergesearannya: "; cin >> geser;
+    enkripsi (inFile, geser);
 }
